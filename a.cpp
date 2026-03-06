47ba43a886bfd7dc601070ba902b9a819a6d9ae2
Arpit Rana <arpitrana35773553@gmail.com>
2026-03-07 00:52:51 +0530

    Added today's DSA practice programs

--- a.cpp
+++ a.cpp
@@ -1,7 +1,7 @@
 
 // SOLVING PATTERN PROBLEMS USING LOOPING STATEMENTS.
 
-///////////////////////////PROBLEM 1///////////////////////
+/////////////////////////PROBLEM 1///////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -16,7 +16,7 @@ int main(){
     }
 }
 
-//////////////////////////PROBLEM 2////////////////////////////
+////////////////////////PROBLEM 2////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -32,7 +32,7 @@ int main(){
     return 0;
 }
 
-//////////////////PROBLEM 3//////////////////////////////////
+////////////////PROBLEM 3//////////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -47,7 +47,7 @@ int main(){
 }
 
 
-/////////////////////PROBLEM 4//////////////////////////////
+///////////////////PROBLEM 4//////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -63,7 +63,7 @@ int main(){
     return 0;
 }
 
-////////////////////////PROBLEM 5 /////////////////////////
+//////////////////////PROBLEM 5 /////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -79,7 +79,7 @@ int main(){
 }
 
 
-///////////////////PROBLEM 6///////////////////
+/////////////////PROBLEM 6///////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -96,7 +96,7 @@ int main(){
 }
 
 
-////////////////////////PROBLEM 7////////////////////////
+//////////////////////PROBLEM 7////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -113,7 +113,7 @@ int main(){
 }
 
 
-///////////////////////////PROBLEM 8////////////////////////
+/////////////////////////PROBLEM 8////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -130,7 +130,7 @@ int main(){
 }
 
 
-///////////////////////////PROBLEM 10/////////////////////////////
+/////////////////////////PROBLEM 10/////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -146,7 +146,7 @@ int main(){
     return 0;
 }
 
-//////////////////////////PROBLEM 11////////////////////////////
+////////////////////////PROBLEM 11////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -166,7 +166,7 @@ int main(){
 }
 
 
-////////////////////////////PROBLEM 12////////////////////////////
+//////////////////////////PROBLEM 12////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -187,7 +187,7 @@ int main(){
 
 
 
-////////////////////////PROBLEM 13///////////////////////////////
+//////////////////////PROBLEM 13///////////////////////////////
 #include <iostream>
 using namespace std;
 int main(){
@@ -206,3 +206,128 @@ int main(){
     }
     return 0;
 }
+
+
+//////////////////FUNCTIONS //////////
+
+//////////////////////////////PROBLEM 1///////////////////
+//1) SUM OF FIRST N NUMBERS
+#include <iostream>
+using namespace std;
+int sum(int n,int sum){
+    for(int i = 1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+int main(){
+    cout<<sum(10,0);
+}
+
+/////////////////////////////////PROBLEM2//////////////////
+// 2) WRITE A FUNCTION TO FIND THE FACTORIAL OF N.
+#include <iostream>
+using namespace std;
+int fact(int n){
+    int facto = 1;
+    for(int i = n;i>=1;i--){
+        facto = i*facto;
+    }
+    return facto;
+}
+
+int main(){
+    cout<<fact(5);
+}
+
+/////////////////////////PROBLEM3////////////////////////
+// 3) CALCULATE THE SUM OF DIGITS OF A NUMBER
+#include <iostream>
+using namespace std;
+int digit(int n,int sum){
+    while(n>0){
+        int last = n%10;
+        n = n/10;
+        sum+=last;
+    }
+    return sum;
+}
+int main(){
+    cout<<digit(373737,0);
+}
+
+
+////////////////////////PROBLEM 4////////////////////
+// 4) CALCULATE nCr BINOMIAL COEFFICIENT FOR n 7 r.
+#include <iostream>
+using namespace std;
+int bin(int n, int r){
+    int facto = 1;
+    for(int i = n;i>=1;i--){
+        facto = facto*i;
+    }
+    int facto2 =1;
+    for(int i =r;i>=1;i--){
+        facto2 = facto2*i;
+    }
+    int facto3 = 1;
+    for(int i = (n-r);i>=1;i--){
+        facto3 = facto3*i;
+
+    }
+    return facto/(facto2*facto3);
+}
+
+int main(){
+    cout<<bin(8,3);
+}
+
+//////////////////////////PROBLEM 5///////////////////////
+//WRITE A FUNCTION TO CHECK IF A NUMBER IS A PRIME OR NOT
+#include <iostream>
+using namespace std;
+char* isPrime(int n){
+    int temp = 0;
+    for(int i = 2;i<n;i++){
+        if(n%i==0)
+            temp+=1;
+    }
+    if(temp>0){
+        return "notPrime";
+    }else{
+        return "Prime";
+    }
+}
+int main(){
+    cout<<isPrime(37);
+}
+
+/////////////////////PROBLEM 6////////////////////////
+// 6) WRITE A FUNCTION TO PRINT ALL PRIME NUMBERS FROM 2 TO N.
+#include <iostream>
+using namespace std;
+
+int isPrime(int n){
+    int temp = 0;
+    for(int i = 2;i<n;i++){
+        if(n%i==0)
+            temp+=1;
+    }
+    if(temp>0){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+int prime(int n){
+    for(int i = 1;i<n;i++){
+        if(isPrime(i)==true)
+            cout<<i<<endl;
+    }
+}
+
+int main(){
+    cout<<prime(50);
+}
+
