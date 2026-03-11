8cda4ea12b23f8c524662c972f62d7e85ce10939
Arpit Rana <arpitrana35773553@gmail.com>
2026-03-11 20:59:12 +0530

    Added today's Problems

--- array.cpp
+++ array.cpp
@@ -67,3 +67,75 @@ int main(){
     return 0;
 }
 
+//////////////11-03-2026//////////////
+/////////////PROBLEM 1///////////////
+//FIND THE INDEX OF THE TARGET VALUE IN AN ARRAY USING LINEAR SEARCH ALGORITHM
+#include <iostream>
+using namespace std;
+int linearsearch(int array[],int size,int target){
+    for(int i = 0;i<size;i++){
+        if(array[i]==target)
+        return i;
+    }
+    return -1;
+}
+
+int main(){
+    int array[]={1,34,57,86,28,14};
+    int target = 2;
+    int size = sizeof(array)/sizeof(int);
+    cout<<linearsearch(array,size,target)<<endl;
+    return 0;
+}
+
+///////////PROBLEM 2////////////
+//REVERSE AN ARRAY WITHOUT MAKING A SECOND ARRAY
+#include <iostream>
+using namespace std;
+void reverse(int arr[],int size){
+    int start = 0;
+    int end = size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int size = sizeof(arr)/sizeof(int);
+    reverse(arr,size);
+
+
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+
+/////////////PROBLEM 3/////////////
+//WRITE A FUNCTION TO CALCULATE SUM & PRODUCT OF ALL NUMBERS IN AN ARRAY
+#include <iostream>
+using namespace std;
+void sumOf(int arr[],int size){
+    int sum = 0;
+    int product = 1;
+    for(int i = 0;i<size;i++){
+        sum += arr[i];
+        product *= arr[i];
+    }
+    cout<<"The Sum Of Numbers In Array Is = "<<sum<<endl;
+    cout<<"The Product Of Numbers In Array Is = "<<product<<endl;
+}
+
+int main(){
+    int arr[5];
+    int size = sizeof(arr)/sizeof(int);
+    cout<<"Enter The Numbers in Array"<<endl;
+    for(int i = 0;i<size;i++){
+        cin >> arr[i];
+    }
+    sumOf(arr,size);
+    return 0;
+}
