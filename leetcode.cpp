8ad75903ba3fc7e2f97c6e3cd9d4f3138e0bbb98
Arpit Rana <arpitrana35773553@gmail.com>
2026-04-02 23:43:55 +0530

    Added today's problem

--- leetcode.cpp
+++ leetcode.cpp
@@ -156,3 +156,38 @@ public:
         return -1;
     }
 };
+
+//////////////02-04-2026/////////
+//////Problem8: Single element in a sorted array
+//////Approach: Binary search, Time complexity O(logN)
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        int start = 0;
+        int end = n - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(mid == 0 && nums[0] != nums[1]) return nums[0];
+            if(mid == n - 1 && nums[n - 1] != nums[n - 2]) return nums[n - 1];
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]){
+                return nums[mid];
+            }
+            if(mid % 2 != 0){
+               if(nums[mid] == nums[mid - 1]){
+                start = mid + 1;
+               }else{
+               end = mid - 1;
+               }
+            }else{
+                if(nums[mid] == nums[mid - 1]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
