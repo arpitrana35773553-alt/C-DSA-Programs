dab652cc9199559728ce27fdb50c378fa6f9ab8d
Arpit Rana <arpitrana35773553@gmail.com>
2026-04-16 22:12:53 +0530

    Added today's Program

--- strings.cpp
+++ strings.cpp
@@ -17,3 +17,39 @@ int main(){
     cout<<"Output: "<<str<<endl;
 }
 
+/////////16-04-2026////////
+////CHECK THE STRING IS PALINDROME OR NOT
+#include <iostream>
+#include <string>
+using namespace std;
+bool isPalindrome(string s){
+    int i = 0;
+    int j = s.length() - 1;
+    while(i < j){
+        if(s[i] == ' '){
+            i++;
+            continue;
+        }
+        if(s[j] == ' '){
+            j--;
+            continue;
+        }
+        char left = tolower(s[i]);
+        char right = tolower(s[j]);
+        if(left != right){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+int main(){
+    string s = "Race car";
+    if(isPalindrome(s)){
+        cout<<"Palindrome"<<endl;
+    }else{
+        cout<<"Not Plaindrome"<<endl;
+    }
+    return 0;
+}
