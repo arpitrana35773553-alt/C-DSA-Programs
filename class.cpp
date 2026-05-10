7bdc121b9e631b9bcc29a45fbe949d71ee1de5bd
Arpit Rana <arpitrana35773553@gmail.com>
2026-05-10 12:21:42 +0530

    Added new programs

--- class.cpp
+++ class.cpp
@@ -84,3 +84,96 @@ int main(){
     return 0;
 }
 
+
+#include <iostream>
+using namespace std;
+class Occurence{
+    private:
+    int num;
+    int digit;
+    public:
+    void input(){
+        cin >> num;
+        cin >> digit;
+    }
+    int countOccurence(){
+        int count = 0;
+        while(num > 0){
+            int check = num % 10;
+            if(check == digit){
+                count++;
+            }
+            num = num / 10;
+        }
+        return count;
+    }
+};
+int main(){
+    Occurence o;
+    o.input();
+    int ans = o.countOccurence();
+    cout << ans;
+}
+
+
+#include <iostream>
+using namespace std;
+class Main{
+    public:
+    string name;
+    int roll;
+    void getting(string n, int r){
+        name = n;
+        roll = r;
+        cout << "Name = "<<name<<" Roll no = "<<roll<<endl;
+    }
+};
+int main(){
+    Main s1;
+    s1.getting("Doraemon",1);
+    s1.getting("Nobita",2);
+    cout<<s1.name<<endl;
+    Main s2;
+    s2.getting("Kavya",3);
+    cout<<s2.name<<endl;
+}
+
+#include <iostream>
+using namespace std;
+class Main{
+    public:
+    void sum(int num , int num2){
+        cout<<"Sum = "<<num + num2;
+    }
+    void multiply(int num, int num2){
+        cout<<"Product = "<< num * num2;
+    }
+    void division(int num , int num2){
+        cout<<"Division = "<<num / num2;
+    }
+    void subtract(int num , int num2){
+        cout<<"Difference = "<<num - num2;
+    }
+};
+int main(){
+   Main m;
+   int num;
+    int num2;
+    char symbol;
+   while(true){
+    cin >> num;
+    cin >> num2;
+    cin >> symbol;
+    if(symbol == '+'){
+        m.sum(num,num2);
+    }else if(symbol == '-'){
+        m.subtract(num,num2);
+    }else if(symbol == '*'){
+        m.multiply(num,num2);
+    }else if(symbol == '/'){
+        m.division(num,num2);
+    }else{
+        return 0;
+    }
+   }
+}
